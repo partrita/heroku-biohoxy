5fcfe5ba026eedec5383350ebbcc8b52d9b14d17
deepthought <partrita@gmail.com>
2020-09-23 12:50:31 +0900

    let it be

--- project/settings.py
+++ project/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'page',
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib import admin
 
 
 urlpatterns = [
+        path('', include('page.urls')),
     path('admin/', admin.site.urls),
 ]
